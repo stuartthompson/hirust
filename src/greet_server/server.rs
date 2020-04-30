f280a13cc2ff344266fb9d82437f58ac62d04680
Stuart Thompson <stuart@stuartthompson.net>
2020-04-30 13:41:36 -0700

    Cleaning up server/message parsing.

--- src/greet_server/server.rs
+++ src/greet_server/server.rs
@@ -1,34 +1,13 @@
-use std::io::{Error, Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::io::Read;
+use std::net::TcpListener;
 use std::thread;
 use std::time::Duration;
 
 const ADDR: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 512;
 
-fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
-    println!("[Server] Incoming connection from: {}", stream.peer_addr()?);
-    let mut buff = [0; 512];
-    loop {
-        let bytes_read = stream.read(&mut buff)?;
-        if bytes_read == 0 {
-            return Ok(());
-        }
-        stream.write(&buff[..bytes_read])?;
-    }
-}
-
 pub fn start_server() {
     let server = TcpListener::bind(ADDR).expect("Listener failed to bind.");
-    for stream in server.incoming() {
-        match stream {
-            Ok(stream) => thread::spawn(move || {
-                handle_client(stream)
-                    .unwrap_or_else(|error| println!("[Server] Error {:?}", error));
-            }),
-            Err(e) => thread::spawn(move || { eprintln!("Failed: {}", e) }),
-        };
-    }
 
     server
         .set_nonblocking(true)
@@ -39,21 +18,19 @@ pub fn start_server() {
     // Live for 5 seconds
     while server_hb < 5 {
         if let Ok((mut socket, addr)) = server.accept() {
-            println!("Client {} connected", addr);
+            println!("[Server] Client {} connected", addr);
 
             let mut buff = vec![0; MSG_SIZE];
 
-            match socket.read_exact(&mut buff) {
+            match socket.read(&mut buff) {
                 Ok(_) => {
-                    println!("[Server] Buffering message.");
-                    // let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                    println!("[Server] Converting to utf-8");
-                    // let msg = String::from_utf8(msg).expect("Invalid uft8 message.");
+                    let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                    let msg = String::from_utf8(msg).expect("Invalid uft8 message.");
 
-                    // println!("{}: {:?}", addr, msg);
+                    println!("[Server] Client {} says: {:?}", addr, msg);
                 }
-                Err(_) => {
-                    println!("[Server] Error reading message from client.");
+                Err(e) => {
+                    println!("[Server] Error reading message from client: {:?}", e);
                 }
             }
         }
