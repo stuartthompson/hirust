adc7bf9e6235521b6d6a3ba2f65244520655c9c8
Stuart Thompson <stuart@stuartthompson.net>
2020-04-30 11:31:25 -0700

    Updating main method to use client/server.

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,27 @@
 
 extern crate chrono;
 mod greeter;
+mod greet_server;
+mod greet_client;
+
+use std::thread;
 
 fn main() {
     // Say hello directly
     println!("Hello, World! I'm Rust, nice to meet you.");
 
-    // Say hello from a function within a module
+    // Say hello from function within a greeter module
     greeter::simple_greeter::say_hello();
-
     greeter::datetime_greeter::say_hello();
+
+    // Start a greeting server
+    let server_thread = thread::spawn(|| {
+        greet_server::server::start_server();
+    });
+
+    // Say hello from the client
+    greet_client::client::say_hello();
+
+    // Wait for server
+    server_thread.join().expect("The server thread panicked.");
 }
